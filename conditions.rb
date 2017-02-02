71841dd8112578b840b823723c10c88ebf035d66
Nauman Hussain <naumanshussain@lewisu.edu>
2017-02-02 17:31:05 +0000

    Thursday class 2/2/17

---
+++ conditions.rb
@@ -0,0 +1,15 @@
+print("What score did you get: ")
+score = gets.chomp.to_i
+if score >= 90
+    grade = "A"
+elsif score >= 80
+    grade = "B"
+elsif score >= 70
+    grade = "C"
+elsif score >= 60
+    grade = "D"
+else
+    grade = "F"
+end
+
+puts "You earned a #{grade}"
